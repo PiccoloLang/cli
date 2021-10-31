f6219a937944d14276e3d1c13ec7c00c905c5d37
Nikos Plugachev <nikos@plugachev.com>
2021-10-31 01:00:03 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "piccolo/include.h"
+#include "piccolo/stdlib/stdlib.h"
+
+void printError(const char* format, va_list args) {
+    vfprintf(stderr, format, args);
+}
+
+int main(int argc, const char** argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Usage: piccolo [filepath]\n");
+        return 1;
+    }
+
+    struct piccolo_Engine engine;
+    piccolo_initEngine(&engine, printError);
+    piccolo_addIOLib(&engine);
+    piccolo_addTimeLib(&engine);
+
+    struct piccolo_Package* package = piccolo_loadPackage(&engine, argv[1]);
+
+    if(!piccolo_executePackage(&engine, package)) {
+        piccolo_enginePrintError(&engine, "Runtime error.\n");
+    }
+
+    piccolo_freeEngine(&engine);
+
+    return 0;
+}
