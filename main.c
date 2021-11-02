f04b116b5f6285b32fc4410be55b75db3f078937
Nikos Plugachev <nikos@plugachev.com>
2021-11-02 19:03:55 -0400

    Added debug stdlib, minor tweaks

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
+#define PICCOLO_ENABLE_DEBUG_LIB
+
 #include "piccolo/include.h"
 #include "piccolo/stdlib/stdlib.h"
+#include "piccolo/debug/disassembler.h"
 
 void printError(const char* format, va_list args) {
     vfprintf(stderr, format, args);
 }
 
 int main(int argc, const char** argv) {
-    if(argc != 2) {
+    if(argc == 1) {
         fprintf(stderr, "Usage: piccolo [filepath]\n");
+        return 1;
+    }
+
+    bool debug = false;
+    bool error = false;
+    for(int i = 2; i < argc; i++) {
+        if(strcmp(argv[i], "-debug") == 0) {
+            debug = true;
+        } else {
+            fprintf(stderr, "Unknown flag %s\n", argv[i]);
+            error = true;
+        }
+    }
+    if(error) {
         return 1;
     }
 
@@ -16,12 +35,19 @@ int main(int argc, const char** argv) {
     piccolo_initEngine(&engine, printError);
     piccolo_addIOLib(&engine);
     piccolo_addTimeLib(&engine);
+    if(debug)
+        piccolo_addDebugLib(&engine);
 
     struct piccolo_Package* package = piccolo_loadPackage(&engine, argv[1]);
     if(package->compilationError) {
         piccolo_freeEngine(&engine);
         return -1;
-    } else if(!piccolo_executePackage(&engine, package)) {
+    }
+
+    if(debug) {
+        piccolo_disassembleBytecode(&package->bytecode);
+    }
+    if(!piccolo_executePackage(&engine, package)) {
         piccolo_enginePrintError(&engine, "Runtime error.\n");
     }
 
