0f2489eb978cb6595712a733b817ba1d22b6b0b3
Nikos Plugachev <nikos@plugachev.com>
2022-02-14 13:12:42 -0500

    Added git package resolving

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
 
 #include "piccolo/include.h"
 #include "piccolo/stdlib/picStdlib.h"
@@ -10,6 +12,8 @@
 void printError(const char* format, va_list args) {
     vfprintf(stderr, format, args);
 }
+
+const char* homePath;
 
 typedef enum {
     CLI_ARG_SHORT,
@@ -132,9 +136,46 @@ static void handler(const cli_argType type, const size_t nArg, const char* const
     }
 }
 
+struct piccolo_Package* findGitPkg(struct piccolo_Engine* engine, struct piccolo_Compiler* compiler, const char* sourceFilepath, const char* name, size_t nameLen) {
+    if(nameLen < 4)
+        return NULL;
+    if(memcmp(name + nameLen - 4, ".git", 4) != 0)
+        return NULL;
+
+    const char* pkgNameEnd = name + nameLen - 4;
+    char pkgNameBuf[PICCOLO_MAX_PACKAGE];
+    char* curr = pkgNameBuf;
+    for(const char* i = name; i < pkgNameEnd; ++i) {
+        if(*i != '/' && *i != ':') {
+            *curr = *i;
+            curr++;
+        }
+    }
+    *curr = '\0';
+
+    char pathBuf[PICCOLO_MAX_PACKAGE];
+    snprintf(pathBuf, PICCOLO_MAX_PACKAGE, "%s/.piccolo/%s", homePath, pkgNameBuf);
+    DIR* dir = opendir(pathBuf);
+    if(dir == NULL) {
+        char cmdBuf[PICCOLO_MAX_PACKAGE];
+        snprintf(cmdBuf, PICCOLO_MAX_PACKAGE, "git clone %.*s --recursive %s &> /dev/null", nameLen, name, pathBuf);
+        if(system(cmdBuf)) {
+            piccolo_enginePrintError(engine, "Could not clone package '%.*s'.\n", nameLen, name);
+            return NULL;
+        }
+    } else {
+        closedir(dir);
+    }
+
+    snprintf(pathBuf, PICCOLO_MAX_PACKAGE, "%s/lib.pic", pkgNameBuf);
+    return piccolo_resolvePackage(engine, compiler, sourceFilepath, pathBuf, strlen(pathBuf));
+}
+
 int main(int argc, const char** argv) {
     struct cli_argInfo argInfo = { 0 };
     cli_parseArgs(argc, argv, handler, sizeof(shortArgs) / sizeof(shortArgs[0]), shortArgs, sizeof(longArgs) / sizeof(longArgs[0]), longArgs, &argInfo);
+
+    homePath = getenv("HOME");
 
     if(argInfo.debug) {
         printf("Package = %s\n", argInfo.package);
@@ -151,6 +192,12 @@ int main(int argc, const char** argv) {
 
     struct piccolo_Engine engine;
     piccolo_initEngine(&engine, printError);
+
+    engine.findPackage = findGitPkg;
+    char pathBuf[PICCOLO_MAX_PACKAGE];
+    snprintf(pathBuf, PICCOLO_MAX_PACKAGE, "%s/.piccolo/", homePath);
+    piccolo_addSearchPath(&engine, pathBuf);
+
     for(register size_t i = 0; i < argInfo.n_paths; ++i) {
         piccolo_addSearchPath(&engine, argInfo.paths[i]);
     }
